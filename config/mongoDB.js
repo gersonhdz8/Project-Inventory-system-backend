9d1dc4254a7f5dd2043c387e2f67816463eb005c
GersonHdz <79285684+gersonhdz8@users.noreply.github.com>
2023-09-18 02:57:20 -0500

    feat: :sparkles: Database connection module is created. (#5)
    Database connection module is created and data is corrected and added to the collections.

--- config/mongoDB.js
+++ config/mongoDB.js
@@ -1,1 +1,17 @@
-import {MongoClient} from "mongodb"
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+dotenv.config();
+
+export default async function connect() {
+    try {
+        const uri = `${process.env.ATLAS_STRING}`;
+        const options = {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        };
+        const client = await new MongoClient(uri, options).connect();
+        return client;
+    } catch (error) {
+        return {status: 500, message: error};
+    }
+}
