004a65cbba2726c20a1b9647fa52126f4fa30234
GersonHdz <gersonhdz8@gmail.com>
2023-09-18 01:13:58 -0500

    feat: :sparkles: Controllers are created for all modules with GET,POST,PUT,DELETE methods.
    The authentication controller is left empty while the complete authentication module is created.

---
+++ controllers/admin.js
@@ -0,0 +1,40 @@
+import Model from "../models/modelMongoDB.js"
+
+export default class AdminController{
+
+    static async getAdmin(req,res,next){
+        try {
+            const admin = await Model.getAdmin(parseInt(req.params.id))            
+            res.status(200).json(admin);
+        } catch (error) {
+            next(error); 
+        }
+    }
+
+    static async getAllAdmin(req,res,next){
+        try {
+            const admin = await Model.getAllAdmin()            
+            res.status(200).json(admin);
+        } catch (error) {
+            next(error); 
+        }
+    }   
+    static async deleteAdmin(req,res,next){
+
+        try {
+            const admin = await Model.deleteAdmin(parseInt(req.params.id))                       
+            res.status(200).json(admin);
+        } catch (error) {
+            next(error); 
+        }
+    }
+    static async updateAdmin(req,res,next){
+
+        try {
+            const admin = await Model.updateAdmin(parseInt(req.params.id),req.body)            
+            res.status(200).json(admin);
+        } catch (error) {
+            next(error); 
+        }
+    }
+}
---
+++ controllers/product.js
@@ -0,0 +1,50 @@
+import Model from "../models/modelMongoDB.js"
+
+export default class Model {
+
+    static async getProduct(req,res,next){
+        try {
+            const product = await Model.getProduct(parseInt(req.params.id))            
+            res.status(200).json(product);
+        } catch (error) {
+            next(error); 
+        }
+    }
+
+    static async getAllProduct(req,res,next){
+        try {
+            const product = await Model.getAllProduct()            
+            res.status(200).json(product);
+        } catch (error) {
+            next(error); 
+        }
+    }
+    static async createProduct(req,res,next){
+
+        try {
+            const product = await Model.createProduct(req.body)
+            res.status(200).json(product);
+        } catch (error) {
+            next(error); 
+        }
+    }
+    static async deleteProduct(req,res,next){
+
+        try {
+            const product = await Model.deleteProduct(parseInt(req.params.id))                       
+            res.status(200).json(product);
+        } catch (error) {
+            next(error); 
+        }
+    }
+    static async updateProduct(req,res,next){
+
+        try {
+            const product = await Model.updateProduct(parseInt(req.params.id),req.body)            
+            res.status(200).json(product);
+        } catch (error) {
+            next(error); 
+        }
+    }
+
+}
